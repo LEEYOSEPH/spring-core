b40f8669770b7dd66bf30c016cd08f0edf74a86a
LEEYOSEPH <laksmi0628@gmail.com>
2022-06-29 20:49:04 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/mamber/MemberServiceIml.java
@@ -0,0 +1,16 @@
+package hello.core.mamber;
+
+public class MemberServiceIml implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberID) {
+        return memberRepository.findById(memberID);
+    }
+}
