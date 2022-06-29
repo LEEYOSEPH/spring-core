0c94470a8b5a6526a2e38e90330b5ae64285a393
LEEYOSEPH <laksmi0628@gmail.com>
2022-06-29 21:35:45 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.mamber.Grade;
+import hello.core.mamber.Member;
+import hello.core.mamber.MemberService;
+import hello.core.mamber.MemberServiceIml;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceIml();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -9,7 +9,7 @@ public class FixDiscountPolicy implements  DiscountPolicy{
 
     @Override
     public int discount(Member member, int price) {
-        if (member.getGrade() == Grade.BASIC) {
+        if (member.getGrade() == Grade.VIP) {
             return discountFixAmount;
         } else {
             return 0;
